TheHorscht/EZMouse

commit 88d40f928609c5b970f5d1dcd0bd6bacdbc584cd

Fix resizing secondardy sides past constraints when resizing cardinal directions

resize.lua
@@ -186,19 +186,17 @@ return function(props, change_left, change_top, change_right, change_bottom, cor
     elseif corner == 8 or corner == 4 then
       scale = desired_scale_x
     end
-    local m1 = 1
-    local m2 = 2
     if secondary.left then
-      scale = clamp(scale, min_scale_left*m1, max_scale_left*m2)
+      scale = clamp(scale, min_scale_left, ((max_scale_left - 1) * 2) + 1)
     end
     if secondary.right then
-      scale = clamp(scale, min_scale_right*m1, max_scale_right*m2)
+      scale = clamp(scale, min_scale_right, ((max_scale_right - 1) * 2) + 1)
     end
-    if true or secondary.top then
-      scale = clamp(scale, min_scale_top*m1, max_scale_top*1)
+    if secondary.top then
+      scale = clamp(scale, min_scale_top, ((max_scale_top - 1) * 2) + 1)
     end
     if secondary.bottom then
-      scale = clamp(scale, min_scale_bottom*m1, max_scale_bottom*m2)
+      scale = clamp(scale, min_scale_bottom, ((max_scale_bottom - 1) * 2) + 1)
     end
 
     desired_scale_x = scale

tests.lua
@@ -438,14 +438,27 @@ end)
 
 test("(aspect) Constraints work when secondary sides hit them", function()
   local props = {
-    x = 50, y = 50,
+    x = 50, y = 20,
     width = 50, height = 100,
     aspect = true,
     constraints = { left = 0, top = 0, right = 200, bottom = 175, }
   }
-  expect(update_draggable(created_merged_table(props, { width = 50, height = 100 }), -25, 0, 0, 0, 8)).to_be(-25, -25, 0, 25)
-  expect(update_draggable(created_merged_table(props, { width = 100, height = 50 }), 0, -100, 0, 0, 2)).to_be(-50, -50, 50, 0)
-  -- TODO: More comprehensive testing...
+  expect(update_draggable(created_merged_table(props, { width = 50, height = 100 }), -25, 0, 0, 0, 8)).to_be(-10, -10, 0, 10)
+  expect(update_draggable(created_merged_table(props, { x = 10, y = 50, width = 100, height = 50 }), 0, -100, 0, 0, 2)).to_be(-5, -5, 5, 0)
+  local props2 = created_merged_table(props, { x = 150, y = 50, constraints = { bottom = 500 } })
+  expect(update_draggable(props2, 0, -100, 0, 0, 2)).to_be(0, 0, 0, 0)
+  local props3 = created_merged_table(props, { x = 100, y = 500, constraints = { bottom = 5000 } })
+  local c = { left = 50, top = 50, right = 300, bottom = 300 }
+  local props3 = { aspect = true, constraints = c,
+    x = 225, y = 200, width = 50, height = 100,
+  }
+  expect(update_draggable(props3, 0, -200, 0, 0, 2)).to_be(-25, -100, 25, 0)
+  local props4 = { aspect = true, constraints = c,
+    x = 230, y = 250, width = 25, height = 50,
+  }
+  local l, t, r, b = update_draggable(props4, 0, -200, 0, 0, 2)
+  local new_right_side_x = props4.x + props4.width + r
+  expect(new_right_side_x).to_be(300)
 end)
 
 test("(aspect) Constraints of secondary side work resizing diagonally", function()
@@ -484,16 +497,6 @@ test("(aspect) Min sizes get respected when resizing", function()
   test(props, 100, 0, 0, -100, 7)
   test(props, 100, 0, 0, 0, 8)
 end)
-
--- test("(aspect) Catch one specific bug about jumpy values when resizing", function()
---   local props = {
---     x = 10, y = 10,
---     width = 25, height = 50,
---     max_width = 50, max_height = 100,
---     aspect = true,
---   }
---   expect(update_draggable(created_merged_table(props, { width = 50, height = 100, max_width = 50, max_height = 50 }), -100, 0, 0, 0, 2)).to_be(0,0 ,0, 0)
--- end)
 
 test("(all) Output is 0 when input is 0", function()
   local props = {

commit e55f76df09bab9d5e01991538d6a15d3c4950e50

Fix bug introduced in 88d40f9286

resize.lua
@@ -187,16 +187,32 @@ return function(props, change_left, change_top, change_right, change_bottom, cor
       scale = desired_scale_x
     end
     if secondary.left then
-      scale = clamp(scale, min_scale_left, ((max_scale_left - 1) * 2) + 1)
+      local s = max_scale_left
+      if secondary.right then
+        s = ((s - 1) * 2) + 1
+      end
+      scale = clamp(scale, min_scale_left, s)
     end
     if secondary.right then
-      scale = clamp(scale, min_scale_right, ((max_scale_right - 1) * 2) + 1)
+      local s = max_scale_right
+      if secondary.left then
+        s = ((s - 1) * 2) + 1
+      end
+      scale = clamp(scale, min_scale_right, s)
     end
     if secondary.top then
-      scale = clamp(scale, min_scale_top, ((max_scale_top - 1) * 2) + 1)
+      local s = max_scale_top
+      if secondary.bottom then
+        s = ((s - 1) * 2) + 1
+      end
+      scale = clamp(scale, min_scale_top, s)
     end
     if secondary.bottom then
-      scale = clamp(scale, min_scale_bottom, ((max_scale_bottom - 1) * 2) + 1)
+      local s = max_scale_bottom
+      if secondary.top then
+        s = ((s - 1) * 2) + 1
+      end
+      scale = clamp(scale, min_scale_bottom, s)
     end
 
     desired_scale_x = scale

tests.lua
@@ -443,8 +443,8 @@ test("(aspect) Constraints work when secondary sides hit them", function()
     aspect = true,
     constraints = { left = 0, top = 0, right = 200, bottom = 175, }
   }
-  expect(update_draggable(created_merged_table(props, { width = 50, height = 100 }), -25, 0, 0, 0, 8)).to_be(-10, -10, 0, 10)
-  expect(update_draggable(created_merged_table(props, { x = 10, y = 50, width = 100, height = 50 }), 0, -100, 0, 0, 2)).to_be(-5, -5, 5, 0)
+  expect(update_draggable(created_merged_table(props, { width = 50, height = 100 }), -25, 0, 0, 0, 8)).to_be(-20, -20, 0, 20)
+  expect(update_draggable(created_merged_table(props, { x = 10, y = 50, width = 100, height = 50 }), 0, -100, 0, 0, 2)).to_be(-10, -10, 10, 0)
   local props2 = created_merged_table(props, { x = 150, y = 50, constraints = { bottom = 500 } })
   expect(update_draggable(props2, 0, -100, 0, 0, 2)).to_be(0, 0, 0, 0)
   local props3 = created_merged_table(props, { x = 100, y = 500, constraints = { bottom = 5000 } })